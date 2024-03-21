f380ad33b8d7971ce6ead2b50bf0ca2c5f75466a
Sami Mäkelä <mrsmkl@gmail.com>
2024-03-21 15:46:43 +0200

    init

---
+++ programs/basics/src/lib.rs
@@ -0,0 +1,296 @@
+#![allow(clippy::result_large_err)]
+
+use anchor_lang::prelude::*;
+use anchor_spl::associated_token::AssociatedToken;
+use anchor_spl::token_interface::{
+    self, Mint, MintTo, TokenAccount, TokenInterface, TransferChecked,
+};
+
+declare_id!("6qNqxkRF791FXFeQwqYQLEzAbGiqDULC5SSHVsfRoG89");
+
+#[program]
+pub mod anchor {
+
+    use super::*;
+
+    #[error_code]
+    pub enum Errors {
+        NoMatch,
+    }
+
+    pub fn create_test(ctx: Context<CreateTest>, cost: u64) -> Result<()> {
+        msg!("Creating something");
+        *ctx.accounts.test_account = Test {
+            owner: ctx.accounts.signer.key(),
+            provider: ctx.accounts.signer.key(),
+            cost,
+        };
+        Ok(())
+    }
+
+    pub fn create_empty(ctx: Context<CreateEmpty>) -> Result<()> {
+        msg!("Creating something");
+        if ctx.accounts.info.provider != ctx.accounts.provider.key() {
+            return err!(Errors::NoMatch);
+        };
+        *ctx.accounts.test_account = TestAccount {
+            consumer: ctx.accounts.signer.key(),
+            provider: ctx.accounts.provider.key(),
+            info: ctx.accounts.info.key(),
+            tokens: 0,
+        };
+        Ok(())
+    }
+
+    pub fn buy_subscription(ctx: Context<BuySubscription>) -> Result<()> {
+        if ctx.accounts.sub.consumer != ctx.accounts.signer.key() {
+            return err!(Errors::NoMatch);
+        };
+        ctx.accounts.sub.tokens += 10;
+        let cpi_accounts = TransferChecked {
+            from: ctx.accounts.consumer_aa.to_account_info().clone(),
+            mint: ctx.accounts.mint.to_account_info().clone(),
+            to: ctx.accounts.provider_aa.to_account_info().clone(),
+            authority: ctx.accounts.signer.to_account_info(),
+        };
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_context = CpiContext::new(cpi_program, cpi_accounts);
+        token_interface::transfer_checked(cpi_context, ctx.accounts.info.cost, ctx.accounts.mint.decimals)?;
+        Ok(())
+    }
+
+
+    pub fn create_token(_ctx: Context<CreateToken>, _token_name: String) -> Result<()> {
+        msg!("Create Token");
+        Ok(())
+    }
+
+    pub fn create_token_account(_ctx: Context<CreateTokenAccount>) -> Result<()> {
+        msg!("Create Token Account");
+        Ok(())
+    }
+    pub fn create_associated_token_account(
+        _ctx: Context<CreateAssociatedTokenAccount>,
+    ) -> Result<()> {
+        msg!("Create Associated Token Account");
+        Ok(())
+    }
+    pub fn transfer_token(ctx: Context<TransferToken>, amount: u64) -> Result<()> {
+        let cpi_accounts = TransferChecked {
+            from: ctx.accounts.from.to_account_info().clone(),
+            mint: ctx.accounts.mint.to_account_info().clone(),
+            to: ctx.accounts.to_ata.to_account_info().clone(),
+            authority: ctx.accounts.signer.to_account_info(),
+        };
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_context = CpiContext::new(cpi_program, cpi_accounts);
+        token_interface::transfer_checked(cpi_context, amount, ctx.accounts.mint.decimals)?;
+        msg!("Transfer Token");
+        Ok(())
+    }
+    pub fn transfer_token2(ctx: Context<TransferToken2>, amount: u64) -> Result<()> {
+        let cpi_accounts = TransferChecked {
+            from: ctx.accounts.from.to_account_info().clone(),
+            mint: ctx.accounts.mint.to_account_info().clone(),
+            to: ctx.accounts.to_ata.to_account_info().clone(),
+            authority: ctx.accounts.signer.to_account_info(),
+        };
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_context = CpiContext::new(cpi_program, cpi_accounts);
+        token_interface::transfer_checked(cpi_context, amount, ctx.accounts.mint.decimals)?;
+        msg!("Transfer Token");
+        Ok(())
+    }
+    pub fn mint_token(ctx: Context<MintToken>, amount: u64) -> Result<()> {
+        let cpi_accounts = MintTo {
+            mint: ctx.accounts.mint.to_account_info().clone(),
+            to: ctx.accounts.receiver.to_account_info().clone(),
+            authority: ctx.accounts.signer.to_account_info(),
+        };
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_context = CpiContext::new(cpi_program, cpi_accounts);
+        token_interface::mint_to(cpi_context, amount)?;
+        msg!("Mint Token");
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(token_name: String)]
+pub struct CreateToken<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub extra: SystemAccount<'info>,
+    #[account(
+        init,
+        payer = signer,
+        mint::decimals = 6,
+        mint::authority = signer.key(),
+        seeds = [b"token-2022-token", signer.key().as_ref(), token_name.as_bytes(), extra.key().as_ref()],
+        bump,
+    )]
+    pub mint: InterfaceAccount<'info, Mint>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Interface<'info, TokenInterface>,
+}
+
+#[derive(Accounts)]
+#[instruction(cost: u64)]
+pub struct CreateTest<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub provider_aa: InterfaceAccount<'info, TokenAccount>,
+    pub mint: InterfaceAccount<'info, Mint>,
+    #[account(
+        init,
+        space= 8 + Test::INIT_SPACE,
+        payer = signer,
+        seeds = [b"test", signer.key().as_ref()],
+        bump,
+    )]
+    pub test_account: Account<'info, Test>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Interface<'info, TokenInterface>,
+}
+
+#[derive(Accounts)]
+pub struct CreateEmpty<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(mut)]
+    pub info: Account<'info, Test>,
+    #[account(mut)]
+    pub provider: SystemAccount<'info>,
+    #[account(
+        init,
+        space= 8 + TestAccount::INIT_SPACE,
+        payer = signer,
+        seeds = [b"test", signer.key().as_ref(), info.key().as_ref()],
+        bump,
+    )]
+    pub test_account: Account<'info, TestAccount>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Interface<'info, TokenInterface>,
+}
+
+#[derive(Accounts)]
+pub struct BuySubscription<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(mut)]
+    pub info: Account<'info, Test>,
+    #[account(mut)]
+    pub sub: Account<'info, TestAccount>,
+    #[account(mut)]
+    pub provider_aa: InterfaceAccount<'info, TokenAccount>,
+    #[account(mut)]
+    pub consumer_aa: InterfaceAccount<'info, TokenAccount>,
+    #[account(mut)]
+    pub mint: InterfaceAccount<'info, Mint>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
+
+#[derive(Accounts)]
+pub struct CreateTokenAccount<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub mint: InterfaceAccount<'info, Mint>,
+    #[account(
+        init,
+        token::mint = mint,
+        token::authority = signer,
+        payer = signer,
+        seeds = [b"token-2022-token-account", signer.key().as_ref(), mint.key().as_ref()],
+        bump,
+    )]
+    pub token_account: InterfaceAccount<'info, TokenAccount>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Interface<'info, TokenInterface>,
+}
+
+#[derive(Accounts)]
+pub struct CreateAssociatedTokenAccount<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub mint: InterfaceAccount<'info, Mint>,
+    #[account(
+        init,
+        associated_token::mint = mint,
+        payer = signer,
+        associated_token::authority = signer,
+    )]
+    pub token_account: InterfaceAccount<'info, TokenAccount>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
+
+#[derive(Accounts)]
+pub struct TransferToken<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(mut)]
+    pub from: InterfaceAccount<'info, TokenAccount>,
+    pub to: SystemAccount<'info>,
+    #[account(
+        init,
+        associated_token::mint = mint,
+        payer = signer,
+        associated_token::authority = to
+    )]
+    pub to_ata: InterfaceAccount<'info, TokenAccount>,
+    #[account(mut)]
+    pub mint: InterfaceAccount<'info, Mint>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
+
+#[derive(Accounts)]
+pub struct TransferToken2<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(mut)]
+    pub from: InterfaceAccount<'info, TokenAccount>,
+    pub to: SystemAccount<'info>,
+    #[account(mut)]
+    pub to_ata: InterfaceAccount<'info, TokenAccount>,
+    #[account(mut)]
+    pub mint: InterfaceAccount<'info, Mint>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
+
+// document
+#[account]
+#[derive(InitSpace)]
+pub struct Test {
+    pub owner: Pubkey,
+    pub provider: Pubkey,
+    pub cost: u64,
+}
+
+// account for consumer (agreement)
+#[account]
+#[derive(InitSpace)]
+pub struct TestAccount {
+    pub provider: Pubkey,
+    pub consumer: Pubkey,
+    pub info: Pubkey,
+    pub tokens: u64,
+}
+
+#[derive(Accounts)]
+pub struct MintToken<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(mut)]
+    pub mint: InterfaceAccount<'info, Mint>,
+    #[account(mut)]
+    pub receiver: InterfaceAccount<'info, TokenAccount>,
+    pub token_program: Interface<'info, TokenInterface>,
+}
+
---
+++ tests/anchor.ts
@@ -0,0 +1,313 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { Anchor } from "../target/types/anchor";
+import { getAccount } from "@solana/spl-token";
+
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
+
+describe("anchor", () => {
+  // Configure the client to use the local cluster.
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.Anchor as Program<Anchor>;
+  const connection = program.provider.connection;
+  const TOKEN_2022_PROGRAM_ID = new anchor.web3.PublicKey(
+    "TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb"
+  );
+  // const payer = provider.wallet as anchor.Wallet;
+  const payer = anchor.web3.Keypair.generate();
+  const receiver = anchor.web3.Keypair.generate();
+  const ATA_PROGRAM_ID = new anchor.web3.PublicKey(
+    "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL"
+  );
+
+  const tokenName = "TestToken";
+  const [mint] = anchor.web3.PublicKey.findProgramAddressSync(
+    [
+      Buffer.from("token-2022-token"),
+      payer.publicKey.toBytes(),
+      Buffer.from(tokenName),
+      receiver.publicKey.toBytes(),
+    ],
+    program.programId
+  );
+  const [payerATA] = anchor.web3.PublicKey.findProgramAddressSync(
+    [
+      payer.publicKey.toBytes(),
+      TOKEN_2022_PROGRAM_ID.toBytes(),
+      mint.toBytes(),
+    ],
+    ATA_PROGRAM_ID
+  );
+
+  const [receiverATA] = anchor.web3.PublicKey.findProgramAddressSync(
+    [
+      receiver.publicKey.toBytes(),
+      TOKEN_2022_PROGRAM_ID.toBytes(),
+      mint.toBytes(),
+    ],
+    ATA_PROGRAM_ID
+  );
+
+  const [testAA] = anchor.web3.PublicKey.findProgramAddressSync(
+    [
+      Buffer.from("test"),
+      payer.publicKey.toBytes(),
+    ],
+    program.programId
+  );
+
+  const [subAA] = anchor.web3.PublicKey.findProgramAddressSync(
+    [
+      Buffer.from("test"),
+      receiver.publicKey.toBytes(),
+      testAA.toBytes(),
+    ],
+    program.programId
+  );
+
+  it("Create Token-2022 Token", async () => {
+    console.log("req1")
+    console.log(await connection.requestAirdrop(receiver.publicKey, 1000000000));
+    console.log("req2")
+    await connection.requestAirdrop(payer.publicKey, 1000000000);
+    await connection.requestAirdrop(payer.publicKey, 1000000000);
+    const tx = new anchor.web3.Transaction();
+
+    await sleep(1000);
+
+    console.log(await connection.getAccountInfo(payer.publicKey));
+    console.log(await connection.getBalance(payer.publicKey, {commitment: "processed"}));
+
+    console.log(receiver.publicKey, payer.publicKey)
+
+    const ix = await program.methods
+      .createToken(tokenName)
+      .accounts({
+        mint: mint, // this is the account to be created
+        signer: payer.publicKey,
+        extra: receiver.publicKey,
+        tokenProgram: TOKEN_2022_PROGRAM_ID,
+      })
+      .instruction();
+
+    tx.add(ix);
+
+    console.log("hmm what")
+    const sig = await anchor.web3.sendAndConfirmTransaction(
+      program.provider.connection,
+      tx,
+      [payer]
+    );
+    console.log("Your transaction signature", sig);
+  });
+
+  it("Initialize payer ATA", async () => {
+    console.log(await connection.getBalance(payer.publicKey));
+    await sleep(200);
+    const tx = new anchor.web3.Transaction();
+
+    const ix = await program.methods
+      .createAssociatedTokenAccount()
+      .accounts({
+        tokenAccount: payerATA,
+        mint: mint,
+        signer: payer.publicKey,
+        tokenProgram: TOKEN_2022_PROGRAM_ID,
+      })
+      .instruction();
+
+    tx.add(ix);
+
+    const sig = await anchor.web3.sendAndConfirmTransaction(
+      program.provider.connection,
+      tx,
+      [payer]
+    );
+    console.log("Your transaction signature", sig);
+  });
+
+
+  it("Mint Token to payer", async () => {
+    console.log(await connection.getBalance(payer.publicKey));
+    await sleep(200);
+    console.log(await connection.getAccountInfo(payerATA));
+
+    console.log("token data", await getAccount(connection, payerATA, undefined, TOKEN_2022_PROGRAM_ID))
+
+    const tx = new anchor.web3.Transaction();
+
+    const ix = await program.methods
+      .mintToken(new anchor.BN(200000000))
+      .accounts({
+        mint: mint,
+        signer: payer.publicKey,
+        receiver: payerATA,
+        tokenProgram: TOKEN_2022_PROGRAM_ID,
+      })
+      .signers([payer])
+      .instruction();
+
+    tx.add(ix);
+
+    const sig = await anchor.web3.sendAndConfirmTransaction(
+      program.provider.connection,
+      tx,
+      [payer]
+    );
+    console.log("Your transaction signature", sig);
+  });
+
+  // Using init in the transfer instruction, as init if needed is bot working with Token 2022 yet.
+  it("Transfer Token", async () => {
+    await sleep(200);
+    console.log("token data", await getAccount(connection, payerATA, undefined, TOKEN_2022_PROGRAM_ID))
+    const tx = new anchor.web3.Transaction();
+
+    const ix = await program.methods
+      .transferToken(new anchor.BN(100))
+      .accounts({
+        mint: mint,
+        signer: payer.publicKey,
+        from: payerATA,
+        to: receiver.publicKey,
+        tokenProgram: TOKEN_2022_PROGRAM_ID,
+        associatedTokenProgram: ATA_PROGRAM_ID,
+        toAta: receiverATA,
+      })
+      .instruction();
+
+    tx.add(ix);
+
+    const sig = await anchor.web3.sendAndConfirmTransaction(
+      program.provider.connection,
+      tx,
+      [payer]
+    );
+    console.log("Your transaction signature", sig);
+  });
+
+  it("Transfer Token (2)", async () => {
+    await sleep(200);
+    console.log("token data", await getAccount(connection, payerATA, undefined, TOKEN_2022_PROGRAM_ID))
+    const tx = new anchor.web3.Transaction();
+
+    const ix = await program.methods
+      .transferToken2(new anchor.BN(1))
+      .accounts({
+        mint: mint,
+        signer: receiver.publicKey,
+        from: receiverATA,
+        to: payer.publicKey,
+        tokenProgram: TOKEN_2022_PROGRAM_ID,
+        associatedTokenProgram: ATA_PROGRAM_ID,
+        toAta: payerATA,
+      })
+      .instruction();
+
+    tx.add(ix);
+
+    const sig = await anchor.web3.sendAndConfirmTransaction(
+      program.provider.connection,
+      tx,
+      [receiver]
+    );
+    console.log("Your transaction signature", sig);
+  });
+
+  it("Create something", async () => {
+    console.log(await connection.requestAirdrop(receiver.publicKey, 1000000000));
+    await connection.requestAirdrop(payer.publicKey, 1000000000);
+    await connection.requestAirdrop(payer.publicKey, 1000000000);
+    const tx = new anchor.web3.Transaction();
+
+    await sleep(1000);
+
+    console.log(await connection.getAccountInfo(payer.publicKey));
+    console.log(await connection.getBalance(payer.publicKey, {commitment: "processed"}));
+
+    console.log(receiver.publicKey, payer.publicKey)
+
+    const ix = await program.methods
+      .createTest(new anchor.BN(12))
+      .accounts({
+        signer: payer.publicKey,
+        testAccount: testAA,
+        providerAa: payerATA,
+        mint: mint,
+      })
+      .instruction();
+
+    tx.add(ix);
+
+    console.log("hmm what")
+    const sig = await anchor.web3.sendAndConfirmTransaction(
+      program.provider.connection,
+      tx,
+      [payer]
+    );
+    console.log("Your transaction signature", sig);
+
+    console.log(await program.account.test.fetch(testAA))
+    console.log(await connection.getAccountInfo(testAA))
+
+    console.log(program.programId)
+  })
+
+  it("create subscription", async () => {
+
+    const tx = new anchor.web3.Transaction();
+    const ix = await program.methods
+    .createEmpty()
+    .accounts({
+      signer: receiver.publicKey,
+      provider: payer.publicKey,
+      info: testAA,
+      testAccount: subAA,
+    })
+    .instruction();
+
+    tx.add(ix);
+
+    console.log("hmm what")
+    const sig = await anchor.web3.sendAndConfirmTransaction(
+      program.provider.connection,
+      tx,
+      [receiver]
+    );
+    console.log(await program.account.testAccount.fetch(subAA))
+
+  });
+
+  it("buy subscription", async () => {
+
+    const tx = new anchor.web3.Transaction();
+    const ix = await program.methods
+    .buySubscription()
+    .accounts({
+      signer: receiver.publicKey,
+      info: testAA,
+      sub: subAA,
+      providerAa: payerATA,
+      consumerAa: receiverATA,
+      mint: mint,
+      tokenProgram: TOKEN_2022_PROGRAM_ID,
+      associatedTokenProgram: ATA_PROGRAM_ID,
+   })
+    .instruction();
+
+    tx.add(ix);
+
+    console.log("hmm what")
+    const sig = await anchor.web3.sendAndConfirmTransaction(
+      program.provider.connection,
+      tx,
+      [receiver]
+    );
+    console.log(await program.account.testAccount.fetch(subAA))
+
+  });
+
+});
